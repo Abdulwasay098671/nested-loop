2c79932744b32d0270d7350f6ef50f0572b64174
Abdulwasay098671 <abdulwasay.09867.1@gmail.com>
2024-10-12 06:51:29 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let num = + prompt("enter any number")
+
+for (let j = 1; j <= num; j++) {
+
+    for (let k = 1; k <= 10; k++)  {
+    
+        document.write(j + "x" +  k + "=" + k * j + "<br />" + "<br />")
+        
+    }
+}
